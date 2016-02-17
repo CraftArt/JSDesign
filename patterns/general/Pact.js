97a1e5db338e6473a581aaf6d9e7967e6a2566fb
vivektha <vivek.thakur@intel.com>
2016-02-17 19:52:51 +0530

    Promise structure added, Impl to follow

---
+++ patterns/general/Pact.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/*global module, require*/
+var Pact = function(){
+    this.done = function(){
+
+    };
+    this.resolve = function(){
+
+    };
+
+    this.failed = function(){
+
+    };
+
+    this.fail = function(){
+
+    };
+};
+
+module.exports = Pact;
